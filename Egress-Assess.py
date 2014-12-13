64fd702c78fb044cc62f08f502d29f439df51b81
Chris Truncer <chris@christophertruncer.com>
2014-12-12 23:23:38 -0500

    Added ftp server code

--- Egress-Assess.py
+++ Egress-Assess.py
@@ -27,97 +27,6 @@ from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 
 
-class GetHandler(BaseHTTPRequestHandler):
-    # Some of the http server code came from Dave Kennedy's AES shell
-    # over http - the server specific code
-
-    # should be performing GET requests Help from
-    # http://pymotw.com/2/BaseHTTPServer/
-    def do_GET(self):
-
-        # 404 since we aren't serving up any pages, only receiving data
-        self.send_response(404)
-        self.end_headers()
-        return
-
-    # handle post request
-    def do_POST(self):
-
-        # Gather the Posted URI from the agent/browser
-        # parsed_path = urlparse.urlparse(self.path)
-        uri_posted = self.path
-        uri_posted = uri_posted.replace("/", "")
-        #incoming_ip = self.client_address[0]
-        # current directory
-        exfil_directory = os.path.join(os.getcwd(), "data")
-        loot_path = exfil_directory + "/"
-
-        # Info for this from -
-        # http://stackoverflow.com/questions/13146064/simple-
-        # python-webserver-to-save-file
-        if uri_posted == "ccdata.php":
-
-            self.send_response(200)
-            self.end_headers()
-            loot_path = exfil_directory + "/"
-
-            # Check to make sure the agent directory exists, and a loot
-            # directory for the agent.  If not, make them
-            if not os.path.isdir(loot_path):
-                os.makedirs(loot_path)
-
-            # Get the date info
-            current_date = time.strftime("%m/%d/%Y")
-            current_time = time.strftime("%H:%M:%S")
-            screenshot_name = current_date.replace("/", "") +\
-                "_" + current_time.replace(":", "") + "ccdata.txt"
-
-            # Read the length of the screenshot file being uploaded
-            screen_length = self.headers['content-length']
-            screen_data = self.rfile.read(int(screen_length))
-
-            # Write out the file
-            with open(loot_path + screenshot_name, 'w') as cc_data_file:
-                cc_data_file.write(screen_data)
-
-        elif uri_posted == "ssndata.php":
-            self.send_response(200)
-            self.end_headers()
-
-            # Check to make sure the agent directory exists, and a loot
-            # directory for the agent.  If not, make them
-            if not os.path.isdir(loot_path):
-                os.makedirs(loot_path)
-
-            # Get the date info
-            current_date = time.strftime("%m/%d/%Y")
-            current_time = time.strftime("%H:%M:%S")
-            screenshot_name = current_date.replace("/", "") +\
-                "_" + current_time.replace(":", "") + "ssndata.txt"
-
-            # Read the length of the screenshot file being uploaded
-            screen_length = self.headers['content-length']
-            screen_data = self.rfile.read(int(screen_length))
-
-            # Write out the file
-            with open(loot_path + screenshot_name, 'w') as cc_data_file:
-                cc_data_file.write(screen_data)
-
-        # All other Post requests
-        else:
-
-            self.send_response(404)
-            self.end_headers()
-
-            print "Odd... someone else is trying to access this web server..."
-            print "Might want to check that out..."
-        return
-
-
-class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
-    pass
-
-
 def cli_parser():
     # Command line argument parser
     parser = argparse.ArgumentParser(
@@ -295,36 +204,6 @@ def ftp_client_connect(command_line_object):
     os.remove(ftp_file_name)
     print "[*] File sent!!!"
     return
-
-
-def ftp_server(command_line_object):
-    # current directory
-    exfil_directory = os.path.join(os.getcwd(), "data")
-    loot_path = exfil_directory + "/"
-
-    # Check to make sure the agent directory exists, and a loot
-    # directory for the agent.  If not, make them
-    if not os.path.isdir(loot_path):
-        os.makedirs(loot_path)
-
-    try:
-        authorizer = DummyAuthorizer()
-        authorizer.add_user(
-            command_line_object.username, command_line_object.password,
-            loot_path, perm="lrw")
-
-        handler = FTPHandler
-        handler.authorizer = authorizer
-
-        # Define a customized banner (string returned when client connects)
-        handler.banner = "Connecting to Egress-Assess's FTP server!"
-
-        server = FTPServer(('', 21), handler)
-        server.serve_forever()
-    except ValueError:
-        print "[*] Error: The directory you provided may not exist!"
-        print "[*] Error: Please re-run with a valid FTP directory."
-        sys.exit()
 
 
 def generate_credit_cards(command_object):
--- protocols/servers/ftp_server.py
+++ protocols/servers/ftp_server.py
@@ -4,6 +4,8 @@ This is the code for the ftp server
 
 '''
 
+import os
+import sys
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
@@ -13,6 +15,36 @@ class Server:
 
     def __init__(self):
         self.protocol = "ftp"
+        self.username = ""
+        self.password = ""
+        self.data_directory = ""
 
     def serve(self):
-        pass
+        # current directory
+        exfil_directory = os.path.join(os.getcwd(), "data")
+        loot_path = exfil_directory + "/"
+
+        # Check to make sure the agent directory exists, and a loot
+        # directory for the agent.  If not, make them
+        if not os.path.isdir(loot_path):
+            os.makedirs(loot_path)
+
+        try:
+            authorizer = DummyAuthorizer()
+            authorizer.add_user(
+                self.username, self.password,
+                loot_path, perm="lrw")
+
+            handler = FTPHandler
+            handler.authorizer = authorizer
+
+            # Define a customized banner (string returned when client connects)
+            handler.banner = "Connecting to Egress-Assess's FTP server!"
+
+            server = FTPServer(('', 21), handler)
+            server.serve_forever()
+        except ValueError:
+            print "[*] Error: The directory you provided may not exist!"
+            print "[*] Error: Please re-run with a valid FTP directory."
+            sys.exit()
+        return
