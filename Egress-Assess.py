95aa06b97a023c9448ea4959ad048a63d94c5d30
Chris Truncer <chris@christophertruncer.com>
2014-12-15 17:42:13 -0500

    Small menu modification

--- Egress-Assess.py
+++ Egress-Assess.py
@@ -78,9 +78,10 @@ def cli_parser():
     if not (
         args.ftp or args.http or args.https or args.http_server or
             args.ftp_server):
-        print "[*] Error: You didn't tell Egress-Assess to act like\
+        print "[*] Error: You didn't tell Egress-Assess to act like \
             a server or client!".replace('    ', '')
         print "[*] Error: Please re-run and provide an action to perform!"
+        parser.print_help()
         sys.exit()
 
     return args
