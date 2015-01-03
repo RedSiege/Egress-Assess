878f422fdedc0a1a265ac6ffec56e00b625c0ef7
Chris Truncer <chris@christophertruncer.com>
2015-01-02 21:41:57 -0500

    Fixed sftp..... :)

--- protocols/servers/sftp_server.py
+++ protocols/servers/sftp_server.py
@@ -27,11 +27,40 @@ class Server:
         # within __init__
         # Anything that needs to be set for the server to run should have
         # a self attribute created within __init__
-        self.protocol = "ssh"
+        self.protocol = "sftp"
         self.username = cli_object.username
         self.password = cli_object.password
         self.sftp_directory = helpers.ea_path() + '/data'
         self.port = 22
+        self.rsa_key = """
+        -----BEGIN RSA PRIVATE KEY-----
+MIIEoQIBAAKCAQEArJqP/6XFBa88x/DUootMmSzYa3MxcTV9FjNYUomqbQlGzuHa
+n1Ef6YClJuBWu1eCdZfoeUoa56du1XV2eGKdDjWEqyie2uZ8RZeJZvT1wCuyvO6X
+E143A4z3xHi6R6Qi7rimJFpxL6lGmYHx64wQgL93FXTe/HrmdPoxGeTEf+PnN/PV
+Se321o/Ludqfu+8cldbuKaYRRZJSPT+sIMafvErL86I3JShYqaBjXcic8yYgaAZx
+6Ieu6A19UJzZurQpCdnWoMMLEQ1EgU4LIkUg+SzVSTpBDV3uiBB0+iOdG+v0v+RO
+53GAcKRx9Y38vQazpdAw4AhX97Hj6c/WcpET+QIBIwKCAQBsfmkkWZHJDxBDKao6
+SO5RpyjzFTUFVNJIeAuhmFx/DSUxlUeXV5Bm4yX7Le1f0JslWCu59BDpYe3lQoT7
+NqvdC7J6NspAc56SJLzEX3Xmgd4QW3Tnmk53QqpePUHj44Or/wlYreC+3240mtKU
+DuXNRSZIAFGmBBvUgAGbP1bxTGRShWlebnDsEFuv8BnrjTB1GBN3SshgwTuApete
+7yPPNNPhiAMHN27z5p5sMDU43+FgZd8GEJbHckmriIcwLr1Q0iwlmsrYRndRnA7u
+bbl9D5SwTROE8mtACHBLOdkJ5glfp68GhKjZ+HPTkI+fKqv70DOB7TsP9F4EsNO/
+FQUzAoGBANo2ScHL6RFHUpztE0+dc9g9Yk4S+tjW1sVHMOWGN/KmwiqBIwiusvWY
+vXf/4i/kbehGnwedAtfRmjQSJbIyOEhMt1MxaN0Wn44YUgoWCbfplJG1Tmk25eEX
+VrwOahTtzDGibtHNNmi97D2dFR7V36mhECTqwyzEE142yGrRJnLPAoGBAMp+YXd/
+D2B0xhFMJmyzYHdBFCQHbm4DWZcGey09tSKo+mei+EDq+knSrjUmJV40PMXwxVjw
+anLZJjEh72e71G28jlR5WEhciT5nJqN5pB8Oc9cHFCGC9mLQrEwW9MYqAz/WvCx4
+lpa1Cge2b/lp7snc3Yt4BfAl35MIqElOg163AoGAGPBC8ZOlm5MfYmQ8uKRHwPES
+jJR0cI2U41iX37eRXY9mpcWdmpefbIZ8DbbYBXkxIdwvbpWZ7Md/Vmh5VjGgCENI
+JsPReFpbYLJShM9RkVyGAgYXlv71s1Mf2vpVRDhvG52JAgjTBKf9veYRCtadN/Um
+ap58tKixwZ/cY/qlTvMCgYBFbSi7QYGdad2CRf6LqzcEUNO0lNVnjB63b++3vWKs
+zDiYj6WSmbTmHFj8R5fIhvBD3YV9lEHA+f53PtW9KnS36OBXeg+jx/SKbIJGrVzX
+cqtfqqfQ+bOPmACPHdBD8SWvfNLNayxQ7Z0J9Wg4QZOy7KO6yhCqG50cd/8vE5rB
+YwKBgQCH9mHpdfORUCXVt1QScw29mhLskx5SA/9vU4lrKpwr0Kkce+d0Cex14jWG
+cLz1fOlcctHsIQBMFxEBR0dM7RNX/kdvWfhiPDl1VgDQIyrAEC9euig92hKhmA2E
+Myw1d5t46XP97y6Szrhcsrt15pmSKD+zLYXD26qoxKJOP9a6+A==
+-----END RSA PRIVATE KEY-----
+"""
 
     def accept_client(self, client, addr, root_dir, users, host_rsa_key, password):
         usermap = {}
@@ -78,14 +107,6 @@ class Server:
 
         print "[*] Starting SFTP Server..."
 
-        # read in the rsa key here
-        with open(os.getcwd() + "/protocols/servers/serverlibs/sftp/key_file.txt", 'r') as rsa_key_file:
-            key_dict = rsa_key_file.readlines()
-
-        rsa_key = ''
-        for line in key_dict:
-            rsa_key = rsa_key + line.strip()
-
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.bind(('0.0.0.0', self.port))
         server_socket.listen(10)
@@ -94,7 +115,7 @@ class Server:
             client, addr = server_socket.accept()
             t = threading.Thread(target=self.accept_client, args=[
                 client, addr, self.sftp_directory, user_map,
-                rsa_key, self.password])
+                self.rsa_key, self.password])
             t.start()
 
         return
