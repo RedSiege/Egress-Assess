5fd00a0f038fd52ae62f023851ce87e395f37dca
Chris Truncer <chris@christophertruncer.com>
2015-02-22 21:07:44 -0500

    Change to dns_resolved server

--- protocols/servers/dns_resolved.py
+++ protocols/servers/dns_resolved.py
@@ -27,7 +27,7 @@ class Server:
             try:
                 print dnsqr_strings
                 spoofed_pkt = IP(dst=packet[IP].src, src=packet[IP].dst) /\
-                      UDP(dport=packet[UDP].sport, sport=packet[UDP].dport) /\
+                      UDP() /\
                       DNS(id=packet[DNS].id, qd=packet[DNS].qd, aa = 1, qr=1, \
                       an=DNSRR(rrname=packet[DNS].qd.qname,  ttl=10, rdata='0.0.0.0'))
                 send(spoofed_pkt)
