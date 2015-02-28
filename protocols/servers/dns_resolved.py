825a8a7bcc7f619dd093bbfe9b8c663c93503e5a
Chris Truncer <chris@christophertruncer.com>
2015-02-27 22:31:52 -0500

    Updated server

--- protocols/servers/dns_resolved.py
+++ protocols/servers/dns_resolved.py
@@ -32,7 +32,11 @@ class Server:
                 else:
                     encoded_data = incoming_data.split('.')[0]
 
-                encoded_data = base64.b64decode(encoded_data)
+                try:
+                    encoded_data = base64.b64decode(encoded_data)
+                except:
+                    pass
+
                 if encoded_data == self.last_packet:
                     pass
                 else:
