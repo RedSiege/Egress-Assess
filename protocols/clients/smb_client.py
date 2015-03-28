4dbe9a7dffcd7a15fe399a0efd0f6678c8c1c839
Chris Truncer <chris@christophertruncer.com>
2015-03-28 16:47:21 -0400

    Updated smb server module and started smb client module

---
+++ protocols/clients/smb_client.py
@@ -0,0 +1,71 @@
+'''
+
+This is the template that should be used for client modules.
+A brief description of the client module can/should be placed
+up here.  All necessary imports should be placed between the
+comments and class declaration.
+
+Finally, be sure to rename your client module to a .py file
+
+'''
+
+import os
+from common import helpers
+
+
+class Client:
+
+    # Within __init__, you have access to everything passed in
+    # via the command line.  self.protocol is the variable listed
+    # when running --list-clients and is what is used in conjunction
+    # with --client <client>.  self.protocol is the only required attribute
+    # of the object.
+    def __init__(self, cli_object):
+        self.protocol = "smb"
+        self.remote_server = cli_object.ip
+        if cli_object.file is None:
+            self.file_transfer = False
+        else:
+            if "/" in cli_object.file:
+                self.file_transfer = cli_object.file.split("/")[-1]
+            else:
+                self.file_transfer = cli_object.file
+
+    # transmit is the only required function within the object.  It is what
+    # called by the framework to transmit data.  However, you can create as 
+    # many "sub functions" for transmit to invoke as needed.  "data_to_transmit"
+    # is a variable passed in by the framework which contains the data that 
+    # is to be sent out by the client.
+    def transmit(self, data_to_transmit):
+
+        # find current directory, make directory for mounting share
+        # current directory
+        exfil_directory = os.path.join(os.getcwd(), "mount")
+        mount_path = exfil_directory + "/"
+
+        # Check to make sure the agent directory exists, and a loot
+        # directory for the agent.  If not, make them
+        if not os.path.isdir(mount_path):
+            os.makedirs(mount_path)
+
+        # Base command to copy file over smb
+        smb_command = "mount -t cifs -o ////" + self.remote_server + "//DATA" + mount_path
+
+        # If using a file, copy it, else write to disk and then copy
+        if not self.file_transfer:
+            smb_file_name = helpers.writeout_text_data(data_to_transmit)
+            smb_full_path = helpers.ea_path() + "/" + smb_file_name
+
+            smb_command += " && cp " + smb_full_path + " " + mount_path
+
+        else:
+            smb_command += " && cp " + self.file_transfer + " " + mount_path
+
+        os.system(smb_command)
+
+        os.system("umount ////" + mount_path)
+        os.system("rm -rf " + mount_path)
+
+        print "[*] File Transmitted!"
+
+        return
--- protocols/servers/smb_server.py
+++ protocols/servers/smb_server.py
@@ -4,6 +4,7 @@ This is the code for the web server
 
 '''
 
+import os
 from impacket import smbserver
 
 
@@ -15,17 +16,28 @@ class Server:
     def serve(self):
         try:
 
+            # current directory
+            exfil_directory = os.path.join(os.getcwd(), "data")
+            loot_path = exfil_directory + "/"
+
+            # Check to make sure the agent directory exists, and a loot
+            # directory for the agent.  If not, make them
+            if not os.path.isdir(loot_path):
+                os.makedirs(loot_path)
+
             server = smbserver.SimpleSMBServer()
 
-            server.addShare("DATA", "data/", "Data Chris share")
+            server.addShare("DATA", "data/", "Egress-Assess data share")
 
             # If you don't want log to stdout, comment the following line
             # If you want log dumped to a file, enter the filename
             server.setLogFile('')
 
+            print "[*] SMB server is currently running..."
+
             # Rock and roll
             server.start()
         # handle keyboard interrupts
         except KeyboardInterrupt:
-            print "[!] Rage quiting, and stopping the web server!"
+            print "[!] Rage quiting, and stopping the smb server!"
         return
