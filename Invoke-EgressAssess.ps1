a51ecf63bb8081543097c6aa75db4f002a988ef0
krystacox <krystacox@gmail.com>
2015-07-07 14:54:23 -0400

    Revert "Update Invoke-EgressAssess.ps1"
    This reverts commit ec133300f1fe460614a042e0b466e3990e9ccbb5.

--- Invoke-EgressAssess.ps1
+++ Invoke-EgressAssess.ps1
@@ -279,7 +279,7 @@ function Invoke-EgressAssess
                     $wc.proxy = $proxy
                 }
                 Write-Verbose  "Uploading  data..."
-                $wc.UploadString($url, $Data.Name)
+                $wc.UploadString($uri, $Data.Name)
                 $totalupload += $sizedata
                 Write-Verbose "Transaction Complete!"
                 break
