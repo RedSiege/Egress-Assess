efd0fb90527ef52e763210da65cb8c8b0649524c
krystacox <krystacox@gmail.com>
2015-07-07 16:07:29 -0400

    Update Invoke-EgressAssess.ps1

--- Invoke-EgressAssess.ps1
+++ Invoke-EgressAssess.ps1
@@ -279,7 +279,7 @@ function Invoke-EgressAssess
                     $wc.proxy = $proxy
                 }
                 Write-Verbose  "Uploading  data..."
-                $wc.UploadString($uri, $Data)
+                $wc.UploadString($uri, "POST", $Data)
                 $totalupload += $sizedata
                 Write-Verbose "Transaction Complete!"
                 break
